75bdd309c7a912d8cc5782e8c19d079b0e9bd5bb
Unknown <bvpenaloza.11@gmail.com>
2018-01-15 13:55:24 -0400

    [ 2 ] Steps for install ( README.md )

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{   
+    printf("\n*** Welcome to DrScratch ***  \n\n");
+    printf("please wait a few seconds.\n");
+    printf("We are starting server... \n");
+    system("python manage.py runserver");
+    system("pause");
+    return 0;
+}
+
+
+
+
